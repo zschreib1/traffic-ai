3783d563e85209850438b962b1881af04b15d9ec
Zoé Schreiber <65547008+zschreib1@users.noreply.github.com>
2023-01-02 14:30:29 +0100

    Add files via upload

---
+++ traffic/traffic.py
@@ -0,0 +1,180 @@
+import cv2
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+from tensorflow import keras
+
+from sklearn.model_selection import train_test_split
+
+EPOCHS = 10
+IMG_WIDTH = 30
+IMG_HEIGHT = 30
+NUM_CATEGORIES = 43
+TEST_SIZE = 0.4
+
+
+def main():
+
+    # Check command-line arguments
+    if len(sys.argv) not in [2, 3]:
+        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+
+    # Get image arrays and labels for all image files
+    images, labels = load_data(sys.argv[1])
+
+    # Split data into training and testing sets
+    labels = tf.keras.utils.to_categorical(labels)
+    x_train, x_test, y_train, y_test = train_test_split(
+        np.array(images), np.array(labels), test_size=TEST_SIZE
+    )
+
+    # Get a compiled neural network
+    model = get_model()
+
+    # Fit model on training data
+    model.fit(x_train, y_train, epochs=EPOCHS)
+
+    # Evaluate neural network performance
+    model.evaluate(x_test,  y_test, verbose=2)
+
+    # Save model to file
+    if len(sys.argv) == 3:
+        filename = sys.argv[2]
+        model.save(filename)
+        print(f"Model saved to {filename}.")
+
+
+def load_data(data_dir):
+    """
+    Load image data from directory `data_dir`.
+
+    Assume `data_dir` has one directory named after each category, numbered
+    0 through NUM_CATEGORIES - 1. Inside each category directory will be some
+    number of image files.
+
+    Return tuple `(images, labels)`. `images` should be a list of all
+    of the images in the data directory, where each image is formatted as a
+    numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
+    be a list of integer labels, representing the categories for each of the
+    corresponding `images`.
+    """
+    #print("Data dir:", data_dir)
+
+    head, tail = os.path.split(data_dir)
+    
+    category_number = 0
+
+    entries = os.listdir(data_dir)
+    #print("Entries:", entries.sort())
+
+    images = []
+    labels = []
+    
+    for cat_number in entries:
+        #print("Category number:", cat_number)
+        cat_path = os.path.join(data_dir, cat_number)
+        #print("Category path:", cat_path)
+
+        img_files = os.listdir(cat_path)
+        
+        for img_file in img_files:
+            img = cv2.imread(os.path.join(data_dir, cat_number, img_file), cv2.IMREAD_UNCHANGED)
+            # print(type(img))
+            # print("Original dimensions:", img.shape)
+            dim = (IMG_WIDTH, IMG_HEIGHT)
+            resized_img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+            # print("Resized dimensions:", resized_img.shape)
+            images.append(resized_img)
+            labels.append(cat_number)
+    
+    res = (images, labels)
+    
+    return res
+
+
+def get_model():
+    """
+    Returns a compiled convolutional neural network model. Assume that the
+    `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
+    The output layer should have `NUM_CATEGORIES` units, one for each category.
+    """
+    """ 
+    # Create a convolutional neural network
+    model = tf.keras.models.Sequential([
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+ 
+        # Max-pooling layer, using 2x2 pool size
+        keras.layers.MaxPooling2D(pool_size=(2, 2)),
+ 
+        # Flatten units
+        keras.layers.Flatten(),
+ 
+        # Add a hidden layer with dropout
+        keras.layers.Dense(128, activation="relu"),
+        # Add a hidden layer with dropout
+        keras.layers.Dense(128, activation="relu"),
+        # Add a hidden layer with dropout
+        keras.layers.Dense(128, activation="relu"),
+        # Add a hidden layer with dropout
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dropout(0.5),
+ 
+        # Add an output layer with output units for all 10 digits
+        keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])    
+    """
+    
+    model = keras.models.Sequential([
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+        keras.layers.Conv2D(
+            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+        keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+ 
+        # Max-pooling layer, using 2x2 pool size
+        keras.layers.MaxPooling2D(pool_size=(2, 2)),
+ 
+        # Flatten units
+        keras.layers.Flatten(),
+ 
+        # Add a hidden layer 
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dense(128, activation="relu"),
+        
+        # Dropout layer
+        keras.layers.Dropout(0.5), 
+    
+        # Add an output layer with output units for NUM_CATEGORIES labels
+        keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ]) 
+    
+
+    model.compile(optimizer="adam",
+                  loss="categorical_crossentropy",
+                  metrics=["accuracy"]
+                    )
+    
+    return model
+
+    #raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
